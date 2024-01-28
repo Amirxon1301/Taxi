a218a099a6768c8741b7a7288c46dd278a405af9
Amirxon1301 <uchihasaske0102@gmail.com>
2024-01-28 19:36:32 +0500

    Taxi loyihasiga swagger document yozdim

--- Taxi/urls.py
+++ Taxi/urls.py
@@ -17,6 +17,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Taxi API uchun yozilgan",
+      default_version='v1',
+      description="",
+      contact= openapi.Contact("Amir Komilonov, Email: uchihasaske0102@gmail.com")
+
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny,],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
